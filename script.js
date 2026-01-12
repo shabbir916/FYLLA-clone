9218410f9ef3b84dfe25ea36b2131e3b306d64bd
Shabbir Sadriwala <sadriwalashabbir671@gmail.com>
2026-01-12 11:29:13 +0530

    Implement responsive navbar with hamburger menu and toggle functionality

--- script.js
+++ script.js
@@ -38,3 +38,10 @@ toggle.addEventListener("click", () => {
     toggleCircle.style.transform = "translateX(0px)";
   }
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
